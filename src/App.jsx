591ef87a631a9c6404725e4c6c4de6c04fdce49e
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-10-20 15:01:48 +0530

    Create a login page and add it in routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import store from "./redux/store"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
