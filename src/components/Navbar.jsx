a47764da942327d92489d3cfd0c86521b4eb00c0
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-10-20 13:54:51 +0530

    Change height of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@ const Navbar = () => {
     const image = ""
     return (
         <motion.div
-        className="w-full px-14 py-5 bg-white flex justify-between items-center"
-        initial={{y: -100, opacity: 0}}
-        animate={{y: 0, opacity: 1}}
-        transition={{duration: 1}}
+            className="w-full h-[76px] px-14 py-5 bg-white flex justify-between items-center"
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
         >
             <h4 className="text-3xl pacifico font-extrabold">Blogger</h4>
             {
-                image ? 
-                <img src="avtar.jpeg" alt="avatar" /> :
-                <button className="bg-red-500 hover:bg-red-600 px-4 text-white text-lg py-1 font-semibold rounded-lg">Login</button>
+                image ?
+                    <img src="avtar.jpeg" alt="avatar" /> :
+                    <button className="bg-red-500 hover:bg-red-600 px-4 text-white text-lg py-1 font-semibold rounded-lg">Login</button>
             }
         </motion.div>
     );
